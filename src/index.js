e5b496c764bd6de21693a8141902b4ee38173b52
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-04-01 22:02:40 -0400

    updated css styling and added final touches

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "27ea1ef443be9864dbtb06do552bd134";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mision is to generate a 4 line poem using basic HTML. Make sure to follow the user instructions.";
+    "You are a romantic poem expert and love to write short poems. Your mision is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
